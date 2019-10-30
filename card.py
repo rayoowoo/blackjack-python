1bb471c4abc90da334f6491a25aec78b4c847f3d
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-30 16:18:42 -0400

    Finish card class

--- card.py
+++ card.py
@@ -1,6 +1,7 @@
 class Card():
+    VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    SUITS = ["spades", "clubs", "diamonds", "hearts"]
+    
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
-
-    
