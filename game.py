7a76e99670c059174a301653b5660d33702d68be
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-31 15:36:50 -0400

    Add all chips to player class

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ class BlackjackGame():
         self.deck = Deck()
         self.dealer = Player()
         self.player = Player()
+        self.pot = []
         
     def deal(self):
         self.dealer.receive(self.deck.deal())
@@ -14,4 +15,20 @@ class BlackjackGame():
         self.player.receive(self.deck.deal())
 
     # figure out game logic
+
+    # player places bet
+    # deal
+    # player goes first. 
+        # can choose to receive another card, "hit", or "stand", and receive no cards. 
+    # dealer goes second.
+        # must hit until 17 or higher. then they must stand. 
     
+    # if the player busts, money goes into the pot. 
+    # if dealer busts, the player with closest to 21 (or 21) wins the pot (usually multiple people). for this game, we'll just double the winnings. 
+    # if nobody busts, player with score closest to 21 wins. 
+
+    # the round keeps going until either the player loses all their money, or the player decides to quit. 
+
+    def place_bet(self, amount):
+        print("You have this many coins:")
+        print(coin.value for coin in self.player.coins)
--- player.py
+++ player.py
@@ -4,12 +4,25 @@ from hand import Hand
 class Player():
     def __init__(self):
         self.hand = Hand()
-        self.money = [
-            Chip(5), Chip(5), Chip(5), Chip(5), Chip(5), Chip(10), Chip(20)
-        ]
+        self.chips = {
+            1: [Chip(1), Chip(1), Chip(1), Chip(1), Chip(1)],
+            5: [Chip(5), Chip(5), Chip(5), Chip(5), Chip(5)],
+            10: [Chip(10)],
+            20: [Chip(20)]
+        }
 
     def receive(self, card):
         self.hand.receive_card(card)
 
     def points(self):
         return self.hand.points
+
+    def all_chips(self):
+        list_of_chips = []
+        for chip in self.chips.values():
+            list_of_chips += chip
+
+        return list(map(lambda chip: chip.value, list_of_chips))
+
+# hi = Player()
+# print(hi.all_chips())
